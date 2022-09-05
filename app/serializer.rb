113cf75c24560a28bcc401e5d3f42f29e7ea3127
Ayush Jain <aayushj.pis@gmail.com>
2022-09-05 15:30:12 +0530

    Modified serializer file to make test passing

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,2 +1,27 @@
+# frozen_string_literal: true
+
+# app/serializer.rb
 class Serializer
+  class << self; attr_reader :attributes end
+
+  attr_reader :object
+
+  def initialize(object)
+    @object = object
+  end
+
+  def self.attribute(name, &block)
+    @attributes ||= {}
+    @attributes[name] = block
+  end
+
+  def serialize
+    attrs = {}
+
+    self.class.attributes.each do |key, proc|
+      attrs[key] = proc.nil? ? object.public_send(key) : instance_eval(&proc)
+    end
+
+    attrs
+  end
 end
